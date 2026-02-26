cae2514dbb2691a3f15ba905990bb8cd6942f398
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-02-26 14:22:08 -0600

    Se crea la UI del mf contador

--- src/app/pages/counter/counter.ts
+++ src/app/pages/counter/counter.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
+  standalone: true,
   imports: [],
   templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
 export class CounterComponent {
+  count = signal(0);
 
+  increment() {
+    this.count.update(v => v + 1);
+  }
+
+  decrement() {
+    this.count.update(v => v - 1);
+  }
+
+  reset() {
+    this.count.set(0);
+  }
 }
